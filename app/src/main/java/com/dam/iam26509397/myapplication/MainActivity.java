7b1baae78d20107ac60d7e6c504fc8c21e44827f
Daniel Amarillo <danielamarillo1@hotmail.com>
2019-01-31 17:50:00 +0100

    imageButtons change img

--- app/src/main/java/com/dam/iam26509397/myapplication/MainActivity.java
+++ app/src/main/java/com/dam/iam26509397/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.dam.iam26509397.myapplication;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    ImageButton imgButton1, imgButton2, imgButton3, imgButton4;
+    private SoundPool soundPool;
+    private int soundID;
+    boolean loaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,18 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        imgButton1 = (ImageButton) findViewById(R.id.imageButton2);
+        imgButton1.setOnClickListener(imgButtonHandler);
+
+        imgButton2 = (ImageButton) findViewById(R.id.imageButton3);
+        imgButton2.setOnClickListener(imgButtonHandler2);
+
+        imgButton3 = (ImageButton) findViewById(R.id.imageButton4);
+        imgButton3.setOnClickListener(imgButtonHandler3);
+
+        imgButton4 = (ImageButton) findViewById(R.id.imageButton5);
+        imgButton4.setOnClickListener(imgButtonHandler4);
+        //setImageResource(R.drawable.ic_action_image_button);
     }
 
     @Override
@@ -41,4 +62,43 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    View.OnClickListener imgButtonHandler = new View.OnClickListener() {
+
+        public void onClick(View v) {
+            //imgButton1.setImageResource(R.drawable.ic_action_image_button);
+            //setBackgroundResource(R.drawable.ic_action_image_button);
+            soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+            /**soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
+                @Override
+                public void onLoadComplete(SoundPool soundPool, int sampleId,
+                                           int status) {
+                    loaded = true;
+                }
+            });*/
+            //soundID = soundPool.load(this, R.raw.sound1, 1);
+        }
+    };
+
+    View.OnClickListener imgButtonHandler2 = new View.OnClickListener() {
+
+        public void onClick(View v) {
+
+        }
+    };
+    View.OnClickListener imgButtonHandler3 = new View.OnClickListener() {
+
+        public void onClick(View v) {
+
+        }
+    };
+
+    View.OnClickListener imgButtonHandler4 = new View.OnClickListener() {
+
+        public void onClick(View v) {
+
+        }
+    };
+
+
 }
