c0d40a4d9c29c80436ad45fdd4af2e40ded505e2
Daniel Amarillo <danielamarillo1@hotmail.com>
2019-01-31 20:09:18 +0100

    sonidos(hay que probarlo) + grabacion (necesita permisos)

--- app/src/main/java/com/dam/iam26509397/myapplication/MainActivity.java
+++ app/src/main/java/com/dam/iam26509397/myapplication/MainActivity.java
@@ -1,25 +1,44 @@
 package com.dam.iam26509397.myapplication;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.media.AudioManager;
+import android.media.MediaRecorder;
 import android.media.SoundPool;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity  {
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
 
     ImageButton imgButton1, imgButton2, imgButton3, imgButton4;
+    //Button startButton, stopButton;
     private SoundPool soundPool;
-    private int soundID;
-    boolean loaded = false;
+    private int soundID1, soundID2, soundID3, soundID4;
+    int loaded = 0;
 
+    MediaRecorder recorder;
+    File audiofile = null;
+    private static final String TAG = "SoundRecordingActivity";
+    private View startButton;
+    private View stopButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +58,22 @@ public class MainActivity extends AppCompatActivity  {
         imgButton4 = (ImageButton) findViewById(R.id.imageButton5);
         imgButton4.setOnClickListener(imgButtonHandler4);
         //setImageResource(R.drawable.ic_action_image_button);
+
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                loaded++;
+            }
+        });
+        soundID1 = soundPool.load(this, R.raw.sound1, 1);
+        soundID2 = soundPool.load(this, R.raw.sound1, 1);
+        soundID3 = soundPool.load(this, R.raw.sound1, 1);
+        soundID4 = soundPool.load(this, R.raw.sound1, 1);
+
+        startButton = findViewById(R.id.button);
+        stopButton = findViewById(R.id.button2);
+
     }
 
     @Override
@@ -68,37 +103,104 @@ public class MainActivity extends AppCompatActivity  {
         public void onClick(View v) {
             //imgButton1.setImageResource(R.drawable.ic_action_image_button);
             //setBackgroundResource(R.drawable.ic_action_image_button);
-            soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
-            /**soundPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
-                @Override
-                public void onLoadComplete(SoundPool soundPool, int sampleId,
-                                           int status) {
-                    loaded = true;
-                }
-            });*/
-            //soundID = soundPool.load(this, R.raw.sound1, 1);
+            AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+            float actualVolume = (float) audioManager
+                    .getStreamVolume(AudioManager.STREAM_MUSIC);
+            float maxVolume = (float) audioManager
+                    .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            float volume = actualVolume / maxVolume;
+            if (loaded == 4) {
+                soundPool.play(soundID1, volume, volume, 0, 0, 1);
+            }
         }
     };
 
     View.OnClickListener imgButtonHandler2 = new View.OnClickListener() {
 
         public void onClick(View v) {
-
+            AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+            float actualVolume = (float) audioManager
+                    .getStreamVolume(AudioManager.STREAM_MUSIC);
+            float maxVolume = (float) audioManager
+                    .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            float volume = actualVolume / maxVolume;
+            if (loaded == 4) {
+                soundPool.play(soundID2, volume, volume, 0, 0, 1);
+            }
         }
     };
     View.OnClickListener imgButtonHandler3 = new View.OnClickListener() {
 
         public void onClick(View v) {
-
+            AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+            float actualVolume = (float) audioManager
+                    .getStreamVolume(AudioManager.STREAM_MUSIC);
+            float maxVolume = (float) audioManager
+                    .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            float volume = actualVolume / maxVolume;
+            if (loaded == 4) {
+                soundPool.play(soundID3, volume, volume, 0, 0, 1);
+            }
         }
     };
 
     View.OnClickListener imgButtonHandler4 = new View.OnClickListener() {
 
         public void onClick(View v) {
-
+            AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+            float actualVolume = (float) audioManager
+                    .getStreamVolume(AudioManager.STREAM_MUSIC);
+            float maxVolume = (float) audioManager
+                    .getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            float volume = actualVolume / maxVolume;
+            if (loaded == 4) {
+                soundPool.play(soundID4, volume, volume, 0, 0, 1);
+            }
         }
     };
 
+    public void startRecording(View view) throws IOException {
 
+        startButton.setEnabled(false);
+        stopButton.setEnabled(true);
+
+        File sampleDir = Environment.getExternalStorageDirectory();
+        try {
+            audiofile = File.createTempFile("sound", ".3gp", sampleDir);
+        } catch (IOException e) {
+            Log.e(TAG, "sdcard access error");
+            return;
+        }
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(audiofile.getAbsolutePath());
+        recorder.prepare();
+        recorder.start();
+    }
+
+    public void stopRecording(View view) {
+        startButton.setEnabled(true);
+        stopButton.setEnabled(false);
+        recorder.stop();
+        recorder.release();
+        addRecordingToMediaLibrary();
+    }
+
+    protected void addRecordingToMediaLibrary() {
+        ContentValues values = new ContentValues(4);
+        long current = System.currentTimeMillis();
+        values.put(MediaStore.Audio.Media.TITLE, "audio" + audiofile.getName());
+        values.put(MediaStore.Audio.Media.DATE_ADDED, (int) (current / 1000));
+        values.put(MediaStore.Audio.Media.MIME_TYPE, "audio/3gpp");
+        values.put(MediaStore.Audio.Media.DATA, audiofile.getAbsolutePath());
+        ContentResolver contentResolver = getContentResolver();
+
+        Uri base = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Uri newUri = contentResolver.insert(base, values);
+
+        sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, newUri));
+        Toast.makeText(this, "Added File " + newUri, Toast.LENGTH_LONG).show();
+    }
 }
